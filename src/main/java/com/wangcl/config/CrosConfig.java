c93fd3544566b3022f7bd77759efbf635725337c
王成龙 <wangchenglong@xiangyulife.com>
2026-01-15 11:51:19 +0800

    允许跨域

---
+++ src/main/java/com/wangcl/config/CrosConfig.java
@@ -0,0 +1,23 @@
+package com.wangcl.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 功能描述：
+ *
+ * @author wangchenglong
+ * @version 1.0
+ * @date 2026/1/15 11:49
+ */
+@Configuration
+public class CrosConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("https://talk-cat.pages.dev") // 允许前端域名
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*");
+    }
+}
